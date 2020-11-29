d2b9154c243dd8b0e2d717965e74c9be33ed41d3
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2020-11-29 09:37:54 +0900

    first_commit

---
+++ call.go
@@ -0,0 +1,7 @@
+package slack_to_twilio_by_cloud_function
+
+import "fmt"
+
+func Call() {
+	fmt.Println("Hello world")
+}
---
+++ call_test.go
@@ -0,0 +1,7 @@
+package slack_to_twilio_by_cloud_function
+
+import "testing"
+
+func TestCall(t *testing.T) {
+	Call()
+}
